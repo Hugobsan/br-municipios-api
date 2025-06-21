5bfddf69ab402c7b7c641e5de3aaf5f99cd72c24
hugobsan <hugocorone10@gmail.com>
2025-06-21 09:22:26 -0300

    add: adicionando provedores de municipios

--- config/services.php
+++ config/services.php
@@ -35,4 +35,8 @@ return [
         ],
     ],
 
+    'municipality_providers' => [
+        'brasilapi' => 'https://brasilapi.com.br/api/ibge/municipios/v1/',
+        'ibge' => 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/',
+    ],
 ];
